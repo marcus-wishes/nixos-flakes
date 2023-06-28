d6e08c2852ae0ce9d17cd0667b2f2a793d9b139a
Markus T <19794318+marcus-wishes@users.noreply.github.com>
2023-06-28 11:22:31 +0200

    added gnome extensions

--- markusnix/markusnix-configuration.nix
+++ markusnix/markusnix-configuration.nix
@@ -247,17 +247,6 @@ in
     enable = true;
   };
 
-  /*fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    jetbrains-mono
-    siji
-    (nerdfonts.override { fonts = [ "Iosevka" "DroidSansMono" "Hack" ]; })
-  ];*/
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
