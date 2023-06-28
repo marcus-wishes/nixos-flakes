1cb4db87739cbf753b0743c3ad52a2e4d5018828
Markus T <19794318+marcus-wishes@users.noreply.github.com>
2023-06-28 11:34:40 +0200

    added a gtk theme to home manager of markus

--- users/markus-home.nix
+++ users/markus-home.nix
@@ -5,4 +5,13 @@
   home.homeDirectory = "/home/markus";
   home.packages = with pkgs; [
   ];
+
+  # set gtk theme
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Materia-dark";
+      package = pkgs.materia-gtk-theme;
+    };
+  };
 }
