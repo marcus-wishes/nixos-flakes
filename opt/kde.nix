cbdee607cda8bbc0f1c46aa637db5ae128334343
Markus T <19794318+marcus-wishes@users.noreply.github.com>
2023-06-29 09:17:06 +0200

    fixed kde

--- opt/kde.nix
+++ opt/kde.nix
@@ -2,7 +2,7 @@
 {
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = false;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # GTK Themes not applied in Wayland fix:
   programs.dconf.enable = true;
